6448d1eab61fc7f0199058192715d7f7dcd4839f
LucasLimaKXY <lukas_kxy@hotmail.com>
2019-02-28 23:27:31 +0000

    Alterando urls

--- quick/index/urls.py
+++ quick/index/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, reverse_lazy
-from .views import Cadastro
+from django.conf.urls import url
+from .views import Cadastro, DashboardProfessor
 from django.contrib.auth.views import (
     LoginView, LogoutView
-
 )
 
 urlpatterns = [
-    path('cadastro/', Cadastro.as_view(), name='cadastro'),
-    path('login/', LoginView.as_view(template_name='index/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('index')), name='logout'),
+    url(r'cadastro/', DashboardProfessor.as_view(),name = 'professor'),
+    url(r'login/', LoginView.as_view(template_name='index/login.html')),
+    url(r'logout/', Cadastro.as_view(), name='cadastro'),
 ]
